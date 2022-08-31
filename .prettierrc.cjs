f9ba1a8cf726e07e2e88bb54972a3959326946a6
Marianne Gomes <mariannegomes02@gmail.com>
2022-08-31 15:25:25 -0300

    Entering eslint, prettier and editorconfig setting

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 8,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    endOfLine: 'auto',
+  }
