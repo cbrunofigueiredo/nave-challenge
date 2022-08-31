074050a40cb9d5af4ab921526636fb7e79dbd3bd
Ítalo Nolasco <italonolasco@Italos-MacBook-Pro.local>
2022-08-31 19:11:56 -0300

    config eslit, prettier and editorconfig

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    trailingComma: 'none',
-    tabWidth: 8,
-    semi: false,
-    singleQuote: true,
-    jsxSingleQuote: true,
-    printWidth: 120,
-    arrowParens: 'avoid',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    endOfLine: 'auto',
-  }
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: 'auto'
+}
